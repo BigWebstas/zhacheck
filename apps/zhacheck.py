3ebe284ca83f3d825dc1b64237e49e988efd0d57
Joshua Webster <2896329+BigWebstas@users.noreply.github.com>
2022-06-30 10:21:54 -0600

    Update zhacheck.py
    Added Token as a config 
    Added IP as a config
    Raised the max time to 120Mins
    Removed Friendly name
    Removed Icon
    Added Replace to set state to clear out old attributes

--- apps/zhacheck.py
+++ apps/zhacheck.py
@@ -6,14 +6,18 @@ import datetime
 
 from websocket import create_connection
 
-ACCESS_TOKEN = "***"
-
 class zha_check(hass.Hass):
 
   def initialize(self):
-    self.listen_event(self.ZHAmonitor, "ZHAcheck")
-    #call this every 60 minutes from an HA automation by action: - event: ZHAcheck
+    self.token = self.args['token']      
+    self.addr = "ws://" + self.args['ip'] + ":8123/api/websocket"
 
+    self.listen_event(self.ZHAmonitor, "ZHAcheck")
+    #call this from an HA automation by action: - event: ZHAcheck
+
+    handle = self.run_every(self.ZHAmonitor, 'now+60', 60*5)
+    #run every 5 mins regardless of event
+    
     self.ZHAdump()
 
   def ZHAmonitor (self, event_name, data, kwargs): 
@@ -22,13 +26,13 @@ class zha_check(hass.Hass):
   def ZHAdump (self): 
     count = 0
     last_seen = {}
-    max_time = 5400 #90 minutes
+    max_time = 7200 #120 minutes
     now = datetime.datetime.now()
 
-    ws = create_connection("ws://192.168.1.*:8123/api/websocket")
+    ws = create_connection(self.addr)
     result =  ws.recv()
 
-    ws.send(json.dumps( {'type': 'auth', 'access_token': ACCESS_TOKEN} ))
+    ws.send(json.dumps( {'type': 'auth', 'access_token': self.token} ))
     result =  ws.recv()
     
     ident = 1
@@ -50,8 +54,8 @@ class zha_check(hass.Hass):
         last_seen[str(device["user_given_name"])] = last_dat3
         count = count + 1
 
-    last_seen['friendly_name'] = 'Last Seen ZHA'
-    last_seen['icon'] = 'mdi:message-alert-outline'
-    self.set_state('sensor.last_seen_zha',state=count,attributes=last_seen)
+#    last_seen['friendly_name'] = 'Last Seen ZHA'
+#    last_seen['icon'] = 'mdi:message-alert-outline'
+    self.set_state('sensor.zha_last_seen', replace=True, state=count, attributes=last_seen)
     #this will create a sensor called sensor.last_seen_zha with a list of attributes containing the name and last_seen time of each sensor not reporting for more then 90 minutes.
     #I then have an automation that emails me an alert when this sensor is greater than 0
